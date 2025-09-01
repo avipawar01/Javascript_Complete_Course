b1772912cb26ad9b9800aa9f1acfa61303a6a39b
example <example@email.com>
2025-09-01 18:42:05 +0530

    first commit

---
+++ 02_data_types.js
@@ -0,0 +1,16 @@
+//number string boolean undefind null 
+
+//.number
+let age=20;
+
+console.log("My age is this: ", age) 
+
+//string
+let a ="Avinash";
+
+console.log("hi,my name is:",a)
+
+//boolean
+
+let b= true;
+console.log(b)
