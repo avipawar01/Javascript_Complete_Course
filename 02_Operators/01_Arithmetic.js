b07f48f472ed5dce7399fc0b16946e227d266265
avipawar01 <avipawar242005@gmail.com>
2025-09-03 19:04:50 +0530

    arithmetic

--- 02_Operators/01_Arithmetic.js
+++ 02_Operators/01_Arithmetic.js
@@ -7,3 +7,38 @@ function add(){
 }
 
 add();
+
+let c =30;
+let d=20;
+function sub(){
+    console.log(c-d);
+}
+
+sub();
+
+let e = 10;
+let f = 20;
+
+function mul(){
+    console.log(e*f);
+}
+
+mul();
+
+let g = 10;
+let h = 20;
+
+function div(){
+    console.log(g/h);
+}
+
+div();
+
+let i =20;
+let j= 15;
+
+function mod(){
+    console.log(i%j);
+}
+
+mod();
