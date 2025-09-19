eca3f76750f611c8d9171e7f082e5d38644a0aaa
avipawar01 <avipawar242005@gmail.com>
2025-09-19 18:21:49 +0530

    Create And Remove DOM

--- 07_DOM/script.js
+++ 07_DOM/script.js
@@ -4,6 +4,11 @@ let submit = document.getElementById('submit');
 let container = document.querySelector('.container')
 
 
+const newDiv = document.createElement('div');
+newDiv.textContent = "Hello World";
+newDiv.setAttribute('class', 'container');
+
+
 function addTodo(){
     let li = document.createElement('li')
     let desc = document.createElement('li');
@@ -12,3 +17,16 @@ function addTodo(){
     container.appendChild(li);
     container.appendChild(desc);
 }
+
+
+container.appendChild(newDiv);
+
+
+
+newDiv.remove();
+
+
+const todo = document.querySelector('.todo');
+const heading = document.getElementsByTagName('h1')[0]
+
+todo.removeChild(heading);
