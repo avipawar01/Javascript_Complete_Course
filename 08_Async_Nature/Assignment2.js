f0e1f611cf81f5b80407230341f37e351383ff67
avipawar01 <avipawar242005@gmail.com>
2025-09-27 18:55:37 +0530

    Assignment2

--- 08_Async_Nature/Assignment2.js
+++ 08_Async_Nature/Assignment2.js
@@ -29,3 +29,50 @@ function callback1(numbers){
 }
 
 processNumbers(numbers, callback1);
+
+
+// 3.Write a function  that filters an array based on a callback function. 
+// The callback should decide whether each element should be included in the returned array.
+
+function filterArray (arr,callback2){
+    let results = [];
+
+    for (let i = 0;i<arr.length;i++){
+        if(callback2(arr[i],i,arr)){
+            results.push(arr[i]);
+        }
+    }
+    return results;
+}
+let numbers1 = [1,2,3,4,5,6];
+
+let evens = filterArray(numbers1,function(num){
+    return num % 2 == 0;
+});
+console.log(evens);
+
+// 4. Asynchronous Callback Simulation
+
+// Write a function simulateAsyncTask that simulates an asynchronous operation (like fetching data).
+//  Once the operation is complete, a callback function should be invoked with a message indicating completion.
+
+function simulateAsyncTask (callback3){
+    console.log("task started");
+
+
+    setTimeout(()=> {
+        console.log("tast completed!");
+        callback3("operation is complete")
+    },2000);
+}
+
+function callback3(message){
+    console.log("callback3 message:",message)
+}
+
+simulateAsyncTask(callback3);
+
+// 5. Nested Callbacks (Advanced)
+//Write a function fetchData that simulates fetching data from a server.
+//Once the data is fetched, pass it to a callback function that processes it further (e.g., transforming or logging each element).
+//Demonstrate chaining of multiple callbacks.
